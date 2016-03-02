5c2c3267ca0f363300cc82f8a794399e9fcb546a
JekaJava <wojda007@ukr.net>
2016-03-02 21:45:55 +0200

    update

---
+++ src/main/java/ua/wojda/Database.java
@@ -0,0 +1,40 @@
+package ua.wojda;
+
+import com.mysql.fabric.jdbc.FabricMySQLDriver;
+
+import java.sql.Connection;
+import java.sql.Driver;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+import java.util.Arrays;
+
+/**
+ * Created by wojda on 02.03.2016.
+ */
+public class Database {
+    private final String URL = "jdbc:mysql://localhost:3306/GYM";
+    private final String USERNAME = "root";
+    private final String PASSWORD = "root";
+    private Connection connection;
+    public void connectToDatabase() throws Exception {
+        try {
+            Driver driver = new FabricMySQLDriver();
+            DriverManager.registerDriver(driver);
+            connection = DriverManager.getConnection(URL, USERNAME, PASSWORD);
+            System.out.println("Connect to database successfully.");
+        } catch (SQLException e) {
+            System.out.println("Error." + Arrays.toString(e.getStackTrace()));
+        }
+    }
+
+    public void databaseIsClosed() throws SQLException {
+        connection.close();
+        if ( connection.isClosed()) {
+            System.out.println("Connection to database closed.");
+        }
+    }
+
+    public static Database newDatabase() {
+        return new Database();
+    }
+}
--- src/main/java/ua/wojda/Main.java
+++ src/main/java/ua/wojda/Main.java
@@ -4,7 +4,9 @@ package ua.wojda;
  * Created by wojda on 29.02.2016.
  */
 public class Main {
-    public static void main(String[] args) {
-
+    public static void main(String[] args) throws Exception {
+        Database database = Database.newDatabase();
+        database.connectToDatabase();
+        database.databaseIsClosed();
     }
 }
